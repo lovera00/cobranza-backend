495b76fb843089d4c2f1d5694924052e1c386e4c
Lovera00 <mathiasalejandroloverasilva@gmail.com>
2023-02-14 20:47:37 -0300

    do not synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { GestionModule } from './gestion/gestion.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
 
     ServeStaticModule.forRoot({
